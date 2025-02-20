a2b9a9c5295001d283ab8e3ca9e986bc2c1c3dd6
Jack Knott <51883714+surelyknott@users.noreply.github.com>
2025-02-20 21:50:50 +0000

    Add solution - CodeHub

---
+++ 8-kyu/beginner-lost-without-a-map/beginner-lost-without-a-map.js
@@ -0,0 +1,13 @@
+function maps(x) {
+  // Pass a function to map and double the values
+  let doubleArr = x.map((num) => num * 2);
+  
+  // Return the new array with doubled values
+  return doubleArr;
+}
+​
+// Test the function
+console.log(maps([4, 5, 7, 9])); 
+​
+​
+​
