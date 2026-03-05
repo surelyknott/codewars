10110c14071db03b1c634a2e9808b86d41b6206d
Jack Knott <51883714+surelyknott@users.noreply.github.com>
2026-03-05 15:46:55 +0000

    Add solution - CodeHub

---
+++ 8-kyu/opposite-number/opposite-number.js
@@ -0,0 +1,8 @@
+const opposite = n => -n
+​
+// PREP
+// p: accepts number
+// r: return opposite / inverse of number
+// e: 1: -1
+// p: return the number that's wrapped in a method to invert it: Math.abs() ... or even easier, just use the - to inverse the negative
+​
