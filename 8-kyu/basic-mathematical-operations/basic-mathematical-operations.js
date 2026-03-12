2011db32e89e7edc1a60ac253bd6a76315e4d6d3
Jack Knott <51883714+surelyknott@users.noreply.github.com>
2026-03-12 23:55:21 +0000

    Add solution - CodeHub

---
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.js
@@ -0,0 +1,17 @@
+function basicOp(operation, value1, value2) {
+​
+  switch (operation) {
+    case '+':
+      return value1 + value2
+​
+    case '-':
+      return value1 - value2
+​
+    case '*':
+      return value1 * value2
+​
+    case '/':
+      return value1 / value2
+  }
+​
+}
