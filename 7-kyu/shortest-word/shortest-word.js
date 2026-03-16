db381f091857dc0fc351c9c14e9646f3e0186bc6
Jack Knott <51883714+surelyknott@users.noreply.github.com>
2026-03-16 16:21:24 +0000

    Update solution - CodeHub

--- 7-kyu/shortest-word/shortest-word.js
+++ 7-kyu/shortest-word/shortest-word.js
@@ -1,27 +1,5 @@
-function findShort(stringOfWords){ 
-  
-  const strToArray = stringOfWords.split(' '); 
-  
-  let counter = strToArray[0].length;
-  
-  for (let i = 0; i < strToArray.length; i++){ 
-    if (strToArray[i].length <= counter){ 
-      counter = strToArray[i].length;
-    }
-  }
-  return counter;
+function findShort(s){
+  return s
+  .split(' ')
+  .reduce((shortest, word) => Math.min(shortest, word.length), Infinity)
 }
-​
-/*
-​
-P - given a string of words
-R - return the length of the shortest word(s)
-E - dancer, sprinter, actor => actor
-P - 
-​
-1. split words into array (done)
-2. loop through or method that checks the shortest length word (done)
-3. return just the length of that word (done)
-4. or word(s) if shared shortest length (done)
-​
-*/
